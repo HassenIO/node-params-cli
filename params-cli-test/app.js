d3f062167d0c3f8459047e27a48d27324869e940
Hassen Taidirt <htaidirt@gmail.com>
2018-11-04 18:58:41 +0100

    Add prepare to package.json

--- params-cli-test/app.js
+++
@@ -1,9 +0,0 @@
-const params = require('params-cli');
-
-console.log(params.script());
-
-console.log(params.has('x'));
-console.log(params.get('x'));
-
-console.log(params.has('y'));
-console.log(params.get('y'));
